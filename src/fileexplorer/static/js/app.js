654635fb996068dc3afe1a906827ba8b2d80cf47
Ryan Hunter <ryanhunter237@gmail.com>
2023-11-07 09:31:07 +0200

    Refactor app.js into separate functions for images and pdfs

--- src/fileexplorer/static/js/app.js
+++ src/fileexplorer/static/js/app.js
@@ -1,38 +1,50 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Get list of thumbnails with CSS class img-preview
     const thumbnails = document.querySelectorAll('.img-preview');
-    // Get the visualization panel. Can toggle d-none class to reveal
     const visPanel = document.getElementById('vis-panel');
-
-    // Access data attributes
     const imageData = document.getElementById('data');
     const imageBaseUrl = imageData.getAttribute('data-image-url');
     const currentDirectory = imageData.getAttribute('data-current-directory');
 
+    function updateVisDisplay(newElement) {
+        const visDisplay = document.getElementById('vis-display');
+        visDisplay.replaceWith(newElement);
+        visPanel.classList.remove('d-none');
+    }
+
+    function displayImage(fullImagePath) {
+        var newImg = document.createElement('img');
+        newImg.id = 'vis-display';
+        newImg.src = `${imageBaseUrl}/${fullImagePath}`;
+        newImg.className = 'img-fluid';
+        updateVisDisplay(newImg);
+    }
+
+    function displayPDF(fullImagePath) {
+        var newEmbed = document.createElement('embed');
+        newEmbed.id = 'vis-display';
+        newEmbed.src = `${imageBaseUrl}/${fullImagePath}#toolbar=0&navpanes=0&scrollbar=0`;
+        newEmbed.type = 'application/pdf';
+        newEmbed.width = '100%';
+        newEmbed.height = '500';
+        updateVisDisplay(newEmbed);
+    }
+
+    function getFullImagePath(element) {
+        const filename = element.getAttribute('data-filename');
+        const fullImagePath = currentDirectory + '/' + filename;
+        return fullImagePath;
+    }
+
     thumbnails.forEach(img => {
         img.addEventListener('click', function () {
-            // Get visualization display to replace with a new one.
-            const visDisplay = document.getElementById('vis-display');
-            // two levels up from the thumbnail is its row which has the data-filename attribute
-            const filename = this.parentElement.parentElement.getAttribute('data-filename');
             const filetype = this.parentElement.parentElement.getAttribute('data-filetype');
-            const fullImagePath = currentDirectory + '/' + filename;
+            const fullImagePath = getFullImagePath(this.parentElement.parentElement);
+
             if (filetype === 'image') {
-                var newImg = document.createElement('img');
-                newImg.id = 'vis-display';
-                newImg.src = `${imageBaseUrl}/${fullImagePath}`;
-                newImg.className = 'img-fluid';
-                visDisplay.replaceWith(newImg);
+                displayImage(fullImagePath);
             } else if (filetype === 'pdf') {
-                var newEmbed = document.createElement('embed');
-                newEmbed.id = 'vis-display';
-                newEmbed.src = `${imageBaseUrl}/${fullImagePath}#toolbar=0&navpanes=0&scrollbar=0`;
-                newEmbed.type = 'application/pdf';
-                newEmbed.width = '100%';
-                newEmbed.height = '500';
-                visDisplay.replaceWith(newEmbed);
+                displayPDF(fullImagePath);
             }
-            visPanel.classList.remove('d-none');
         });
     });
 });
