80f2a551d9d3f4111546f6f82e0d3fdcad4726a6
Ryan Hunter <ryanhunter237@gmail.com>
2023-11-15 08:59:16 +0200

    Move functions out of DOMContentLoaded event listener

--- src/fileexplorer/static/js/app.js
+++ src/fileexplorer/static/js/app.js
@@ -1,36 +1,36 @@
 import { updateVisDisplay } from './visDisplay.js'
 import { displaySTL } from './stlViewer.js'
 
+function displayImage(imageUrl) {
+    var newImg = document.createElement('img');
+    newImg.src = imageUrl;
+    newImg.className = 'img-fluid';
+    updateVisDisplay(newImg);
+}
+
+function displayPDF(pdfUrl) {
+    var newEmbed = document.createElement('embed');
+    newEmbed.src = `${pdfUrl}#toolbar=0&navpanes=0&scrollbar=0`;
+    newEmbed.type = 'application/pdf';
+    newEmbed.style.width = '100%';
+    newEmbed.style.height = '100%';
+    updateVisDisplay(newEmbed);
+}
+
+function getFileUrl(filename) {
+    const currentDirData = document.getElementById('current-dir-data');
+    const fileServingUrl = currentDirData.getAttribute('file-serving-url');
+    return fileServingUrl + '/' + filename
+}
+
 document.addEventListener('DOMContentLoaded', function () {
-    function displayImage(imageUrl) {
-        var newImg = document.createElement('img');
-        newImg.src = imageUrl;
-        newImg.className = 'img-fluid';
-        updateVisDisplay(newImg);
-    }
-
-    function displayPDF(pdfUrl) {
-        var newEmbed = document.createElement('embed');
-        newEmbed.src = `${pdfUrl}#toolbar=0&navpanes=0&scrollbar=0`;
-        newEmbed.type = 'application/pdf';
-        newEmbed.style.width = '100%';
-        newEmbed.style.height = '100%';
-        updateVisDisplay(newEmbed);
-    }
-
-    function getFileUrl(element) {
-        const currentDirData = document.getElementById('current-dir-data');
-        const fileServingUrl = currentDirData.getAttribute('file-serving-url');
-        const filename = element.getAttribute('data-filename');
-        return fileServingUrl + '/' + filename
-    }
-
     const thumbnails = document.querySelectorAll('.img-preview');
     thumbnails.forEach(img => {
         img.addEventListener('click', function () {
             const rowElement = this.parentElement.parentElement;
             const filetype = rowElement.getAttribute('data-filetype');
-            const fileUrl = getFileUrl(rowElement);
+            const filename = rowElement.getAttribute('data-filename');
+            const fileUrl = getFileUrl(filename);
 
             if (filetype === 'image') {
                 displayImage(fileUrl);
